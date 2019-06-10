3e92628a907fb1a3bfc3645ea6c5b28b25f921f6
Iga <iga.supinska@gmail.com>
2019-06-10 10:16:59 +0200

    scripts for using external memory

---
+++ task_5/run_working.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+qemu-system-x86_64 -curses -drive file=minix_working.img -enable-kvm -localtime -net user,hostfwd=tcp::10022-:22 -net nic,model=virtio -m 1024M -drive file=extra.img,format=raw,index=1,media=disk
